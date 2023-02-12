6ff5fc8a88e4efa9620b0a704df49fd57d4706cf
Eric Yun <yunseric@gmail.com>
2023-02-12 00:35:07 -0800

    added new font and adding link components

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,7 +10,7 @@ const Nav = () => {
       <div className="nav-container">   
         <div className="navbar">
           <div className= {navOpen ? "logo caret-transparent text-[#fff]" : "logo caret-transparent"}>
-            ERIC YUN</div>
+            YUNIVERSE</div>
           <div className="menu-toggle" onClick = {() => setNavOpen(!navOpen)}>
             <div className = {navOpen ? "hamBox hamBoxOpen" : "hamBox"}>
               <span className = {navOpen ? "lineTop spin bg-[#18181c]" : "lineTop"}>
@@ -24,6 +24,36 @@ const Nav = () => {
           top: navOpen ? "0" : "-100%",
           transitionDelay: navOpen ? "0s" : "0s"
         }}></div>
+        <ul className = "nav-links">
+          <li className = "nav-item">
+            <Link to ="/">Home</Link>  
+            <div className='nav-item-wrapper'></div>
+          </li>
+          <li className = "nav-item">
+            <Link to ="/projects">Projects</Link>  
+            <div className='nav-item-wrapper'></div>
+          </li>
+          <li className = "nav-item">
+            <Link to ="/aboutme">About</Link>  
+            <div className='nav-item-wrapper'></div>
+          </li>
+          <li className = "nav-item">
+            <Link to ="/contact">Contact</Link>  
+            <div className='nav-item-wrapper'></div>
+          </li>
+        </ul>
+        <div className = "nav-footer">
+          <div className="location">
+            <span>Los Angeles, CA</span>
+          </div>
+          <div className="nav-social-media">
+            <ul>
+              <li>
+                <a href="#" target={'_blank'}>YouTube</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>                            
     </div>
   )
