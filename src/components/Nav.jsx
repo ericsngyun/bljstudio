bae88d50cb9ccd34bb5efe36c54a3ed5ed541029
ericsngyun <yunseric@gmail.com>
2023-02-13 22:39:08 -0800

    added nav-menu changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,7 +10,7 @@ const Nav = () => {
       <div className="nav-container">   
         <div className="navbar">
           <div className= {navOpen ? "logo caret-transparent text-[#fff]" : "logo caret-transparent"}>
-            YUNIVERSE</div>
+            ERICYUN</div>
           <div className="menu-toggle" onClick = {() => setNavOpen(!navOpen)}>
             <div className = {navOpen ? "hamBox hamBoxOpen" : "hamBox"}>
               <span className = {navOpen ? "lineTop spin bg-[#18181c]" : "lineTop"}>
@@ -26,36 +26,66 @@ const Nav = () => {
         }}>
           <ul className = "nav-links text-[#fff]">
             <li className = "nav-item">
-              <Link to ="/" onClick = {() => setNavOpen(!navOpen)} style = {{
+              <Link to ="/" 
+              onClick = {() => setNavOpen(!navOpen)} 
+              style = {{
                 top: navOpen ? "0" : "120px",
-                transitionDelay: navOpen ? "0.8s" : "0s"
-              }}>Home</Link>  
+                transitionDelay: navOpen ? ".8s" : "0s"
+              }}>HOME</Link>  
               <div className='nav-item-wrapper'></div>
             </li>
             <li className = "nav-item">
-              <Link to ="/projects">Projects</Link>  
+              <Link to ="/projects" onClick = {() => setNavOpen(!navOpen)} style = {{
+                top: navOpen ? "0" : "120px",
+                transitionDelay: navOpen ? ".8s" : "0s"
+              }}>PROJECTS</Link>  
               <div className='nav-item-wrapper'></div>
             </li>
             <li className = "nav-item">
-              <Link to ="/aboutme">About</Link>  
+              <Link to ="/aboutme" onClick = {() => setNavOpen(!navOpen)} style = {{
+                top: navOpen ? "0" : "120px",
+                transitionDelay: navOpen ? ".8s" : "0s"
+              }}>ABOUT</Link>  
               <div className='nav-item-wrapper'></div>
             </li>
-            <li className = "nav-item">
-              <Link to ="/contact">Contact</Link>  
+            <li className = "nav-item" >
+              <Link to ="/contact" onClick = {() => setNavOpen(!navOpen)} style = {{
+                top: navOpen ? "0" : "120px",
+                transitionDelay: navOpen ? ".8s" : "0s"
+              }}>CONTACT</Link>  
               <div className='nav-item-wrapper'></div>
             </li>
           </ul>
           <div className = "nav-footer text-[#fff]">
-            <div className="location">
-              <span>Los Angeles, CA</span>
+            <div className="location text-[#fff]"
+              style = {{
+                bottom: navOpen ? "0" : "-20px",
+                opacity: navOpen ? "1" : "0",
+                transitionDelay: navOpen ? "1.2s" : "0s",
+              }}
+            >
+              <span>LOS ANGELES, CA</span>
             </div>
             <div className="nav-social-media">
               <ul>
                 <li>
-                  <a href="#" target={'_blank'}>YouTube</a>
+                  <a href="https://www.linkedin.com/in/ericsungyun/" target={'_blank'} 
+                    style = {{
+                      bottom: navOpen ? "0" : "-20px",
+                      opacity: navOpen ? "1" : "0",
+                      transitionDelay: navOpen ? "1.2s" : "0s",
+                    }}>
+                      LINKEDIN
+                  </a>
                 </li>
                 <li>
-                  <a href="#" target={'_blank'}>Instagram</a>
+                  <a href="https://www.instagram.com/ericsyun/" target={'_blank'} style = {{
+                    bottom: navOpen ? "0" : "-20px",
+                    opacity: navOpen ? "1" : "0",
+                    transitionDelay: navOpen ? "1.2s" : "0s",
+                    }}>
+                    INSTAGRAM
+                  </a>
                 </li>
               </ul>
             </div>
