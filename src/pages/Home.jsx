6fc73be5c5a41921b552e318de6f359e4d20f39f
ericsngyun <yunseric@gmail.com>
2023-03-04 23:06:08 -0800

    working on changes to gsap animation intro

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Nav from './Nav'
+import Nav from '../components/Nav'
 
 function Home() {
   return (
     <>
       <Nav/>
+      
       {/* <div className = "grid h-screen place-items-center text-4xl">Home</div> */}
     </>
   )
