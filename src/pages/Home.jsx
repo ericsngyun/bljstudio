6bf0b53a934f5636eeba25c9eb65afbcfc43218c
ericsngyun <yunseric@gmail.com>
2023-03-08 23:28:06 -0800

    changes made to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,14 @@ function Home() {
   return (
     <>
       <Nav/>
-      
+      <div className = "h-[95vh] bg-[#fff]">
+        <div className = "flex herosection">
+          <div>
+
+          </div>
+        </div>
+
+      </div>
       {/* <div className = "grid h-screen place-items-center text-4xl">Home</div> */}
     </>
   )
